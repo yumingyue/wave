693c8ce426335ed9c6672b921aa4ddc0e51e601e
283178170@qq.com <ljh@ljh.(none)>
2015-10-16 21:15:55 +0800

    增加了bug信息调试

--- utils/debug.h
+++ utils/debug.h
@@ -1,5 +1,8 @@
 #ifndef DEBUG_H
 #define DEBUG_H
+
+#define HASHEDID8_FORMAT %x %x %x %x %x %x %x %x
+#define HASHEDID8_VALUE(n) n[0],n[1],n[2],n[3],n[4],n[5],n[6],n[7] 
 enum{
   MSG_DEBUG,MSG_INFO,MSG_WARNING,MSG_ERROR  
 };
